9feb85ed82165c5005d3518b676738f066fb628e
cdpacker <email.wm.edu>
2021-11-21 20:00:26 -0500

    Implemented backend for buttons in Winning and Losing activities.

--- app/src/main/java/edu/wm/cs/cs301/amazebychasepacker/gui/LosingActivity.java
+++ app/src/main/java/edu/wm/cs/cs301/amazebychasepacker/gui/LosingActivity.java
@@ -1,56 +1,97 @@
 package edu.wm.cs.cs301.amazebychasepacker.gui;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.activity.OnBackPressedCallback;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-import edu.wm.cs.cs301.amazebychasepacker.databinding.ActivityLosing2Binding;
 
 import edu.wm.cs.cs301.amazebychasepacker.R;
 
 public class LosingActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
-    private ActivityLosing2Binding binding;
 
     private int pathLength = 0;
-    private int energyConsumed = 3400;
+    private float energyConsumed = 3500;
+
+    private String LosingMessage1 = "Emergency!";
+    private String FailureMessage = "Vehicle has run out of Energy";
+    private String LosingMessage2 = "Attempting Recovery...";
+
+    //Gui////
+    TextView pathText;
+    TextView energyText;
+    Button titleButton;
+
+    TextView losingCause;
+    //////////////
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityLosing2Binding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        setContentView(R.layout.activity_losing);
 
-        setSupportActionBar(binding.toolbar);
+        pathLength = getIntent().getIntExtra("path", 0);
+        energyConsumed = getIntent().getFloatExtra("Consumption", 3500);
+        int cause = getIntent().getIntExtra("Failure", 0);
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_losing2);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        pathText = (TextView) findViewById(R.id.PathTextLosing);
+        energyText = (TextView) findViewById(R.id.EnergyTextLosing);
+        titleButton = (Button) findViewById(R.id.LoseToTitleButton);
+        losingCause = (TextView) findViewById(R.id.FailureCauseText);
 
-        binding.fab.setOnClickListener(new View.OnClickListener() {
+        String msgPath = "Pathlength:  " + pathLength;
+        String msgEnergy = "Energy Consumed:  " + energyConsumed;
+
+        pathText.setText(msgPath);
+        energyText.setText(msgEnergy);
+
+        if(cause == 1)
+        {
+            losingCause.setText("Collision with Asteroid");
+        }
+        else
+        {
+            losingCause.setText("Ran out of Energy");
+        }
+
+
+        titleButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                switchToTitle();
             }
         });
+
+        OnBackPressedCallback callback = new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                //interrupt generation
+                switchToTitle();
+            }
+        };
+
+
     }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_losing2);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
+
+    private void switchToTitle()
+    {
+        Intent toTitle = new Intent (this, AMazeActivity.class);
+        startActivity(toTitle);
     }
 }
--- app/src/main/java/edu/wm/cs/cs301/amazebychasepacker/gui/WinningActivity.java
+++ app/src/main/java/edu/wm/cs/cs301/amazebychasepacker/gui/WinningActivity.java
@@ -1,17 +1,23 @@
 package edu.wm.cs.cs301.amazebychasepacker.gui;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.activity.OnBackPressedCallback;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+
+import org.w3c.dom.Text;
 
 import edu.wm.cs.cs301.amazebychasepacker.databinding.ActivityWinningBinding;
 
@@ -20,37 +26,63 @@ import edu.wm.cs.cs301.amazebychasepacker.R;
 public class WinningActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
-    private ActivityWinningBinding binding;
 
     private int pathLength = 0;
-    private int energyConsumed = 3400;
+    private float energyConsumed = 3400;
+
+    //GUI////
+    TextView pathText;
+    TextView energyText;
+    Button titleButton;
+    ///////////
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityWinningBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        setContentView(R.layout.activity_winning);
 
-        setSupportActionBar(binding.toolbar);
+        pathLength = getIntent().getIntExtra("path", 0);
+        energyConsumed = getIntent().getFloatExtra("Consumption", 3400);
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_winning);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        pathText = (TextView) findViewById(R.id.PathText);
+        energyText = (TextView) findViewById(R.id.EnergyText);
+        titleButton = (Button) findViewById(R.id.WinToTitleButton);
 
-        binding.fab.setOnClickListener(new View.OnClickListener() {
+        String msgPath = "Pathlength:  " + pathLength;
+        String msgEnergy = "Energy Consumed:  " + energyConsumed;
+
+        //energyconsumed will be negative if coming from Manual
+        //If we come from manual, energyConsumed is irrelevant, so don't show it.
+        if(energyConsumed < 0)
+        {
+            energyText.setVisibility(View.INVISIBLE);
+        }
+
+        pathText.setText(msgPath);
+        energyText.setText(msgEnergy);
+
+        titleButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                switchToTitle();
             }
         });
+
+        OnBackPressedCallback callback = new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                //interrupt generation
+                switchToTitle();
+            }
+        };
+
     }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_winning);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
+
+    private void switchToTitle()
+    {
+        Intent toTitle = new Intent (this, AMazeActivity.class);
+        startActivity(toTitle);
     }
 }
