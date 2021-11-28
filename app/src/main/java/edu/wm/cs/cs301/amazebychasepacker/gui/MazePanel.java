fa3cc4aa701fa41a57f5dac17aa089b2892d2e6b
cdpacker <email.wm.edu>
2021-11-28 16:28:00 -0500

    Initial implementation of some MazePanel methods, changes to come

--- app/src/main/java/edu/wm/cs/cs301/amazebychasepacker/gui/MazePanel.java
+++ app/src/main/java/edu/wm/cs/cs301/amazebychasepacker/gui/MazePanel.java
@@ -1,9 +1,20 @@
 package edu.wm.cs.cs301.amazebychasepacker.gui;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.graphics.drawable.PaintDrawable;
 import android.view.View;
 
 public class MazePanel extends View implements P5Panel21 {
+
+    private Canvas art = new Canvas();
+
+    private Paint paint = new Paint();
+
+    private Bitmap map = null;//Bitmap.createBitMap(....)
 
 
     public MazePanel(Context context) {
@@ -16,22 +27,25 @@ public class MazePanel extends View implements P5Panel21 {
 
     @Override
     public void commit() {
-
+        draw(art);
     }
 
     @Override
     public boolean isOperational() {
-        return false;
+
+        return(art != null && paint != null && map != null);
     }
 
     @Override
     public void setColor(int rgb) {
 
+        paint.setColor(rgb);
     }
 
     @Override
-    public int getColor() {
-        return 0;
+    public int getColor()
+    {
+        return paint.getColor();
     }
 
     @Override
@@ -41,21 +55,44 @@ public class MazePanel extends View implements P5Panel21 {
 
     @Override
     public void addFilledRectangle(int x, int y, int width, int height) {
+        art.drawRect(x, y, width, height, paint);
 
     }
 
     @Override
     public void addFilledPolygon(int[] xPoints, int[] yPoints, int nPoints) {
 
+        float[] points = new float[2 * nPoints];
+
+        points = fillPoints(xPoints, yPoints, nPoints, points);
+
+       art.drawPoints(points, paint);
+    }
+
+    private float[] fillPoints(int[] xPoints, int[] yPoints, int nPoints, float[] points) {
+
+        int count = 0;
+        for(int i = 0; i < nPoints; i++)
+        {
+            points[count] = (float) xPoints[i];
+            count++;
+            points[count] = (float) yPoints[i];
+            count++;
+        }
+
+        return points;
     }
 
     @Override
     public void addPolygon(int[] xPoints, int[] yPoints, int nPoints) {
 
+        art.drawPoints(null, 0, 0, paint);
+
     }
 
     @Override
     public void addLine(int startX, int startY, int endX, int endY) {
+        art.drawLine(startX, startY, endX, endY, paint);
 
     }
 
@@ -66,6 +103,7 @@ public class MazePanel extends View implements P5Panel21 {
 
     @Override
     public void addArc(int x, int y, int width, int height, int startAngle, int arcAngle) {
+
 
     }
 
