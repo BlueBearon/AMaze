8635907a1890e41e7a4d3cda9accdbd73f0e5c2e
cdpacker <cdpacker@email.wm.edu>
2021-12-07 12:46:04 -0500

    Edited SensorStatusWidget to be compatible for drawing with panel

--- app/src/main/java/edu/wm/cs/cs301/amazebychasepacker/gui/SensorStatusWidget.java
+++ app/src/main/java/edu/wm/cs/cs301/amazebychasepacker/gui/SensorStatusWidget.java
@@ -10,176 +10,136 @@ import android.view.View;
 
 import androidx.annotation.Nullable;
 
-public class SensorStatusWidget extends View {
+public class SensorStatusWidget{
 
 
     private int[] robotStatus = {1, 0, 0, 1};
     private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
 
 
-    private int centerX; // x coordinate of center point
-    private int centerY; // y coordinate of center point
-    private int size; // size of robot
+    private int x;
+    private int y;
+    private int length;
+    private MazePanel panel;
 
-    public SensorStatusWidget(Context context) {
-        super(context);
-    }
 
-    public SensorStatusWidget(Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
-    }
-
-    public SensorStatusWidget(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-    }
-
-    public SensorStatusWidget(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
-        super(context, attrs, defStyleAttr, defStyleRes);
-    }
-
-    /**
-     * Sets the center position for the compass rose and its size
-     * @param x The x coordinate of the center point
-     * @param y The x coordinate of the center point
-     * @param size The size of the compass rose
-     */
-    public void setPositionAndSize(int x, int y, int size) {
-        centerX = x;
-        centerY = y;
-        this.size = size;
-    }
-
-    @Override
-    protected void onDraw(Canvas canvas)
+    public void NewConstructor(MazePanel panel, int startposx, int startposy, int length, int[] status)
     {
-        super.onDraw(canvas);
-        int width = getWidth();
+        this.panel = panel;
+        x = startposx;
+        y = startposy;
+        this.length = length;
+        robotStatus = status;
+    }
 
-        int rwidth = (int) (.25 * width);
 
+    public void drawWidget()
+    {
+        panel.setColor(Color.YELLOW);
 
-        int startposx = 400;
-        int startposy = 400;
-
-        paint.setStyle(Paint.Style.FILL);
-        paint.setColor(Color.YELLOW);
-
-        canvas.drawRect(startposx, startposy, startposx + rwidth, startposy + rwidth, paint);
-        drawFrontSensor(canvas, startposx, startposy, rwidth);
-        drawLeftSensor(canvas, startposx, startposy, rwidth);
-        drawBackSensor(canvas, startposx, startposy, rwidth);
-        drawRightSensor(canvas, startposx, startposy, rwidth);
+        panel.addFilledRectangle(x, y, x + length, y + length);
+        drawFrontSensor(x, y, length);
+        drawLeftSensor(x, y, length);
+        drawBackSensor(x, y, length);
+        drawRightSensor(x, y, length);
 
     }
 
-    private void drawBackSensor(Canvas canvas, int startx, int starty, int length) {
+    private void drawBackSensor(int startx, int starty, int length) {
         if(robotStatus[3] == 1)
         {
-            paint.setColor(Color.GREEN);
+            panel.setColor(Color.GREEN);
         }
         else
         {
-            paint.setColor(Color.RED);
+            panel.setColor(Color.RED);
         }
 
-        paint.setStrokeWidth(10);
-        paint.setStyle(Paint.Style.FILL_AND_STROKE);
 
-        int half = (int) .5 * length;
+        int[] xpoints = new int[3];
+        int[] ypoints = new int [3];
+        xpoints[0] =  (startx + (int)(.5 * length));
+        ypoints[0] = starty + length;
+        xpoints[1] = startx;
+        ypoints[1] = ((starty + length) + 100);
+        xpoints[2] = startx + length;
+        ypoints[2] = (starty + length) + 100;
 
-        Path cone = new Path();
-        cone.moveTo((startx + (int)(.5 * length)), starty + length);
-        cone.lineTo(startx, (starty + length) + 100);
-        cone.lineTo(startx+length, (starty + length) + 100);
-        cone.lineTo((startx + (int)(.5 * length)), starty + length);
-        cone.close();
-        cone.setFillType(Path.FillType.EVEN_ODD);
-        paint.setStrokeWidth(8);
-        paint.setPathEffect(null);
-        paint.setStyle(Paint.Style.FILL_AND_STROKE);
-        canvas.drawPath(cone, paint);
+        panel.addFilledPolygon(xpoints, ypoints, 3);
     }
 
-    private void drawLeftSensor(Canvas canvas, int startx, int starty, int length) {
-        if(robotStatus[1] == 1)
+    private void drawLeftSensor(int startx, int starty, int length) {
+
+
+        if(robotStatus[3] == 1)
         {
-            paint.setColor(Color.GREEN);
+            panel.setColor(Color.GREEN);
         }
         else
         {
-            paint.setColor(Color.RED);
+            panel.setColor(Color.RED);
         }
 
-        paint.setStrokeWidth(10);
-        paint.setStyle(Paint.Style.FILL_AND_STROKE);
 
-        int half = (int) .5 * length;
+        int[] xpoints = new int[3];
+        int[] ypoints = new int [3];
+        xpoints[0] =  startx;
+        ypoints[0] = (starty + (int)(.5 * length));
+        xpoints[1] = startx-100;
+        ypoints[1] = starty + length;
+        xpoints[2] = startx-100;
+        ypoints[2] = starty;
 
-        Path cone = new Path();
-        cone.moveTo(startx, (starty + (int)(.5 * length)));
-        cone.lineTo(startx-100, starty + length);
-        cone.lineTo(startx-100, starty);
-        cone.lineTo(startx, (starty + (int)(.5 * length)));
-        cone.close();
-        paint.setStrokeWidth(8);
-        paint.setPathEffect(null);
-        paint.setStyle(Paint.Style.FILL_AND_STROKE);
-        canvas.drawPath(cone, paint);
+        panel.addFilledPolygon(xpoints, ypoints, 3);
     }
 
-    private void drawRightSensor(Canvas canvas, int startx, int starty, int length) {
-        if(robotStatus[2] == 1)
+    private void drawRightSensor(int startx, int starty, int length) {
+
+        if(robotStatus[3] == 1)
         {
-            paint.setColor(Color.GREEN);
+            panel.setColor(Color.GREEN);
         }
         else
         {
-            paint.setColor(Color.RED);
+            panel.setColor(Color.RED);
         }
 
-        paint.setStrokeWidth(10);
-        paint.setStyle(Paint.Style.FILL_AND_STROKE);
 
-        int half = (int) .5 * length;
+        int[] xpoints = new int[3];
+        int[] ypoints = new int [3];
+        xpoints[0] =  startx+ length;
+        ypoints[0] = (starty + (int)(.5 * length));
+        xpoints[1] = (startx + length) + 100;
+        ypoints[1] = starty + length;
+        xpoints[2] = (startx + length) + 100;
+        ypoints[2] = starty;
 
-        Path cone = new Path();
-        cone.moveTo(startx+ length, (starty + (int)(.5 * length)));
-        cone.lineTo((startx + length) + 100, starty + length);
-        cone.lineTo((startx + length) + 100, starty);
-        cone.lineTo(startx + length, (starty + (int)(.5 * length)));
-        cone.close();
-        paint.setStrokeWidth(8);
-        paint.setPathEffect(null);
-        paint.setStyle(Paint.Style.FILL_AND_STROKE);
-        canvas.drawPath(cone, paint);
+        panel.addFilledPolygon(xpoints, ypoints, 3);
     }
 
-    private void drawFrontSensor(Canvas canvas, int startx, int starty, int length) {
+    private void drawFrontSensor(int startx, int starty, int length) {
 
-        if(robotStatus[0] == 1)
+
+        if(robotStatus[3] == 1)
         {
-            paint.setColor(Color.GREEN);
+            panel.setColor(Color.GREEN);
         }
         else
         {
-            paint.setColor(Color.RED);
+            panel.setColor(Color.RED);
         }
 
-        paint.setStrokeWidth(10);
-        paint.setStyle(Paint.Style.FILL_AND_STROKE);
 
-        int half = (int) .5 * length;
+        int[] xpoints = new int[3];
+        int[] ypoints = new int [3];
+        xpoints[0] =  (startx + (int)(.5 * length));
+        ypoints[0] = starty;
+        xpoints[1] = startx;
+        ypoints[1] = starty-100;
+        xpoints[2] = startx + length;
+        ypoints[2] = starty-100;
 
-        Path cone = new Path();
-        cone.moveTo((startx + (int)(.5 * length)), starty);
-        cone.lineTo(startx, starty-100);
-        cone.lineTo(startx+length, starty-100);
-        cone.lineTo((startx + (int)(.5 * length)), starty);
-        cone.close();
-        paint.setStrokeWidth(8);
-        paint.setPathEffect(null);
-        paint.setStyle(Paint.Style.FILL_AND_STROKE);
-        canvas.drawPath(cone, paint);
+        panel.addFilledPolygon(xpoints, ypoints, 3);
 
 
 
